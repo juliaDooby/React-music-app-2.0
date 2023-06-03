739bee6cb4662de555e8d3142f555ca7715e9a22
Julia Miroshnichenko22 <jumelie@yandex.ru>
2023-06-04 01:47:35 +0300

    Fetching API

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,32 @@
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+  const genreTitle = 'Pop';
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-between w-full mt-4 mb-10 sm:flex-row">
+        <h2 className="font-bold text-left text-white text-3x1">Discover {genreTitle}</h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="p-3 mt-5 text-sm text-gray-300 bg-black rounded-lg outline-none sm:mt-0"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard key={song.key} song={song} i={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
