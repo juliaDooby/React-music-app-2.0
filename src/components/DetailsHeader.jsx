39963b628e49655b75b16af7544c730763d3f709
Julia Miroshnichenko22 <jumelie@yandex.ru>
2023-08-25 23:49:32 +0300

    HeaderDetails

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,20 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ artistId, artistData, songData }) => {
+
+  return (
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from transparent to-black sm:h-48 h-28" />
+
+    <div className="absolute inset-0 flex items-center">
+      <img alt="art" src={artistId ? artistData?.artists[artistId].attributes?.artwork?.url.replace({'w'}, '500').replace({'h'}, '500')
+    : songData?.images?.coverart 
+    } className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"  />
+
+    <div className="ml-5">
+    <p>{artistId ? artistData?.artists[artistId].attributes.name}</p></div>
+    </div>
+  </div>
+)};
 
 export default DetailsHeader;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -18,7 +18,9 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/track' }),
+    getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}` }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery , useGetSongDetailsQuery} = shazamCoreApi;
+1 / 48 / 30;!!!!!
